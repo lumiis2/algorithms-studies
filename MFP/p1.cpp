d7cffa8ed472a378dfe89907c587fe8b222e164e
lumiis2 <lumiisprog2@gmail.com>
2025-04-27 21:06:25 -0300

    MFP

---
+++ MFP/p1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define f first
+#define s second 
+#define pb push_back
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> freq(m+2, 0); // +2 pra evitar problemas de índice
+    
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+    
+    vector<int> b(m+2, 0);
+    
+    // Processar de trás pra frente
+    for (int k = m; k >= 1; k--) {
+        b[k] = freq[k] + b[k+1];
+    }
+    
+    for (int k = 1; k <= m; k++) {
+        cout << b[k] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
